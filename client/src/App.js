970d5f85d686b54ca676a9a8c8dd7088b58f1ebf
Jessica Greene <jessica.greene89@gmail.com>
2022-11-10 18:30:15 -0500

    stored yelpApiKey in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //THIS IS THE YELP API KEY!
+  const yelpApiKey = process.env.REACT_APP_API_KEY_YELP
+  console.log('The Yelp API Key is defined in App.js.  You can find it at the top of the App component:  ' + yelpApiKey)
+
   return (
     <div className="App">
       <header className="App-header">
