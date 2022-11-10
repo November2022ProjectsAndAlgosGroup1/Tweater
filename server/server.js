a58e694c8275148218e153f9117fb673e0c5eb97
samcohen127 <104338465+samcohen127@users.noreply.github.com>
2022-11-09 17:28:00 -0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const PORT = 8000
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+//require config file
+require('./config/mongoose.config')
+
+//Middleware for formatting and allowing POST requests
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//connect front-end to back-end
+app.use(
+    cors({
+        origin: "http://localhost:3000", credentials: true
+    }),
+)
+
+// import routes
+
+
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`)
+})
+
+
