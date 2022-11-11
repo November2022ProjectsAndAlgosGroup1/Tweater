f6484569ac396ac8a1ccc42f1c4a5c6f336ace3c
Jessica Greene <jessica.greene89@gmail.com>
2022-11-10 19:54:16 -0500

    Testing Development > Jessica branch

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //THIS IS THE YELP API KEY
+  //THIS IS THE YELP API KEY!!!
   // const yelpApiKey = process.env.REACT_APP_API_KEY_YELP
   // console.log('The Yelp API Key is defined in App.js.  You can find it at the top of the App component:  ' + yelpApiKey)
 
