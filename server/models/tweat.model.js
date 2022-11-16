513a8c640cd2d372aff63edaff0142a70e538007
Andrew Stimson <andrew@applejuice.codes>
2022-11-15 16:47:19 -0800

    Fixed decodedJWT null error

--- server/models/tweat.model.js
+++ server/models/tweat.model.js
@@ -1,60 +1,61 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const TweatSchema = mongoose.Schema(
-  {
-    userID: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: [true, "User ID is required"],
-      ref: "User"
-    },
+    {
+        userID: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: [true, "User ID is required"],
+            ref: "User",
+        },
 
-    //This is a Map. I'm not really sure what that means, and I'm too tired to figure it out tonight
-    restaurantInfo: {
-      name: {
-        type: String,
-        required: [true, "Restaurant name is required"],
-      },
-      latitude: {
-        type: Number,
-        required: [true],
-      },
-      logitude: {
-        type: Number,
-        required: [true],
-      },
+        //This is a Map. I'm not really sure what that means, and I'm too tired to figure it out tonight
+        restaurantInfo: {
+            yelpID: String,
+            name: {
+                type: String,
+                required: [true, "Restaurant name is required"],
+            },
+            latitude: {
+                type: Number,
+                required: [true],
+            },
+            logitude: {
+                type: Number,
+                required: [true],
+            },
+        },
+        text: {
+            type: String,
+        },
+        replies: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Reply",
+            },
+        ],
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Like",
+            },
+        ],
+        retweat: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Tweat",
+            },
+        ],
+        originalTweat: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tweat",
+        },
+        // image: {
+        //     //to be implemented
+        // },
     },
-    text: {
-      type: String,
-    },
-    replies: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Reply",
-      },
-    ],
-    likes: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Like",
-      },
-    ],
-    retweat: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Tweat"
-      },
-    ],
-    originalTweat: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Tweat",
-    },
-    // image: {
-    //     //to be implemented
-    // },
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+)
 
-const Tweat = mongoose.model("Tweat", TweatSchema);
+const Tweat = mongoose.model("Tweat", TweatSchema)
 
-module.exports = { Tweat };
+module.exports = { Tweat }
