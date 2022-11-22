c6f1d58d3151e32683f6b1bb40db99953a3bbe2b
samcohen127 <104338465+samcohen127@users.noreply.github.com>
2022-11-22 10:39:24 -0800

    fixed user avatars

--- client/src/components/UserAvatar.js
+++ client/src/components/UserAvatar.js
@@ -7,19 +7,7 @@ const UserAvatar = ({ user, size }) => {
                 <Avatar
                     size={size}
                     name={user && user.name}
-                    src={
-                        user.userName === "iamsam"
-                            ? "/images/sam.jpeg"
-                            : user.userName === "parisa"
-                            ? "/images/parisa.jpeg"
-                            : user.userName === "NOTjessie"
-                            ? "/images/jgreen.jpeg"
-                            : user.userName === "ajstimson"
-                            ? "/images/andrew.jpeg"
-                            : user.userName === "peewee"
-                            ? "/images/peewee.jpeg"
-                            : "/images/bean.jpg"
-                    }
+                    src={"./images/avatar.png"}
                 />
             </WrapItem>
         </Wrap>
